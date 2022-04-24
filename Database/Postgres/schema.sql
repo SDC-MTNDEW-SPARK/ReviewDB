af29c094efedf9ecf69fbc3addd9321ca9fdc747
Dora Xia <duruoxia1992@gmail.com>
2022-04-24 12:45:58 -0700

    finished queries

--- Database/Postgres/schema.sql
+++ Database/Postgres/schema.sql
@@ -1,8 +1,23 @@
+-- SELECT *
+-- FROM pg_stat_activity
+-- WHERE datname = 'reviews';
+
+-- SELECT
+-- 	pg_terminate_backend (pg_stat_activity.pid)
+-- FROM
+-- 	pg_stat_activity
+-- WHERE
+-- 	pg_stat_activity.datname = 'reviews';
+
+-- DROP DATABASE IF EXISTS reviews;
+
+-- CREATE DATABASE reviews;
+
 CREATE TABLE IF NOT EXISTS reviewTable (
-  review_id INT PRIMARY KEY,
+  review_id SERIAL PRIMARY KEY,
   product_id INT NOT NULL,
   rating INT,
-  date DATE,
+  date VARCHAR,
   summary VARCHAR,
   body VARCHAR,
   recommend BOOLEAN,
@@ -14,21 +29,43 @@ CREATE TABLE IF NOT EXISTS reviewTable (
 );
 
 CREATE TABLE IF NOT EXISTS reviewPhotoTable (
-  photo_id INT NOT NULL PRIMARY KEY,
+  photo_id SERIAL PRIMARY KEY,
   review_id INT NOT NULL REFERENCES reviewTable,
   url VARCHAR
 );
 
-
 CREATE TABLE IF NOT EXISTS charReview (
-  id INT NOT NULL PRIMARY KEY,
-  characteristic_id INT NOT NULL REFERENCES characteristic,
+  id SERIAL PRIMARY KEY,
+  characteristic_id INT NOT NULL,
   review_id INT NOT NULL REFERENCES reviewTable,
   value INT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS charTable (
-  id INT NOT NULL PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   product_id INT NOT NULL,
   name VARCHAR NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS characteristic(
+  id SERIAL PRIMARY KEY,
+  characteristic_id INT NOT NULL,
+  product_id INT NOT NULL,
+  review_id INT NOT NULL,
+  name VARCHAR NOT NULL,
+  value VARCHAR NOT NULL
+);
+
+INSERT INTO characteristic(characteristic_id, product_id, review_id, name, value) SELECT charreview.characteristic_id, chartable.product_id,charreview.review_id, chartable.name, charreview.value FROM charreview INNER JOIN chartable ON chartable.id = charreview.characteristic_id;
+
+CREATE INDEX product_characteristic ON characteristic(product_id);
+
+-- -- ALTER TABLE characterTable ADD CONSTRAINT FOREIGN KEY (review_id) REFERENCES reviewTable(review_id);
+
+
+-- -- SELECT charreview.characteristic_id, charreview.review_id, charreview.value, chartable.product_id, chartable.name FROM charreview LEFT JOIN chartable ON chartable.id = charreview.characteristic_id WHERE chartable.product_id is NULL limit 100;
+
+SELECT setval('reviewtable_review_id_seq', (SELECT MAX(review_id) FROM reviewTable)+1);
+SELECT setval('reviewphototable_photo_id_seq', (SELECT MAX(photo_id) FROM reviewPhotoTable)+1);
+INSERT INTO characteristic (review_id, product_id, characteristic_id, value, name)
+SELECT DISTINCT '5774984', '65613', '219308',  3, characteristic.name FROM characteristic WHERE characteristic.characteristic_id = '219308' AND characteristic.product_id = '65613';
